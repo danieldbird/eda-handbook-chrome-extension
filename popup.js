3104dadace7fcf1316e6bbba337fcf9e2246410c
HobbesWSL <jessewilson@outlook.com>
2022-02-19 22:25:44 +1300

    refactor to make creating themes easier

--- popup.js
+++ popup.js
@@ -8,145 +8,116 @@ const changeLightBtn = document.getElementById("changeLightBtn");
 const colorSelector = document.querySelector("#colorRange");
 
 //add listeners
-changeLightBtn.addEventListener("click", () => changeTheme(changeLight));
-changeDarkBtn.addEventListener("click", () => changeTheme(changeDark));
-colorSelector.addEventListener("input", (e) => changeTheme(changeSelectedColor, e.target.value));
+changeLightBtn.addEventListener("click", () => changeTheme( { reset : true } ));
+changeDarkBtn.addEventListener("click", () => changeTheme( { theme : darkTheme } ));
+// colorSelector.addEventListener("input", (e) => changeTheme({ customTemplate = { templateFunction : colorThemeTemplate, templateArgs : e.target.value } }));
 
 
   /*-   -   -   -   -   -   -   */
   /*      Main Functions        */
   /*-   -   -   -   -   -   -   */
 
-// pass themFunction to eda website
-async function changeTheme(themeFunction, ...args) {
+// pass addTheme and theme to EDA website and run with given args
+async function changeTheme(...args) {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   if (tab.url.match(/.*handbook\.eda\.nz.*/)) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: themeFunction,
+      func: applyTheme,
       args: [...args],
     });
-  } else { // if not on eda website
+  } else {
     let popup = document.querySelector('body');
     popup.innerHTML = '';
     popup.textContent = "Only works on handbook.eda.nz";
   }
 }
 
-// create style element
-const styleElement = document.createElement("style");
-styleElement.setAttribute("id", "eda-test");
-document.getElementsByTagName("head")[0].appendChild(styleElement);
+// add create the theme css add add/update <style> tags  - or remove style tags
+function applyTheme ({reset = false, theme=null, customTemplate = {templateFunction: null, templateArgs: null}}) {
+  //if reset = true passed, remove style element and exit function
+  if (reset) { 
+    getStyleEl() && getStyleEl().remove();
+    location.reload();
+    return;
+  };
+
+  //if customTemplate has been added, call templateFunction(template Args) to create themeCSS, otherwise use theme
+  let themeCSS;
+  if (customTemplate.templateFunction && customTemplate.templateArgs) {
+    themeCSS = customTemplate.templateFunction( customTemplate.templateArgs );
+  } else {
+    themeCSS = createCSS(theme);
+  }
+  // use theme to create css and add to the DOM
+  addStyleElement(themeCSS);
+
+  //-- Functions to add to 
+
+  //add <style> element with theme css to the DOM
+  function addStyleElement (css) {
+    let styleEl = getStyleEl();
+    if (!styleEl) {
+      styleEl = document.createElement('style');
+      styleEl.classList.add(".eda-chrome-extension-styles");
+      document.querySelector('head').appendChild(styleEl);
+    };
+    styleEl.innerHTML = css;
+  }
+
+  // use standard template with theme object to create CSS
+  function createCSS (theme) {
+    let { primary, secondary, tertiary, accent, textPrimary, textSecondary } = theme;
+    return (`
+      body, main { background: ${secondary} !important; }
+      .navBarDefault, aside, .previousBtn, .nextBtn { background: ${primary} !important; }
+      p , li, ul, a, h1, h2, h3, h4, h5 { color: ${textSecondary} !important; }
+      table, thead, tr, td, th { background: ${secondary}; color: ${textPrimary}; }
+      ::-webkit-scrollbar { width: 1em; background: ${tertiary}; }
+      //::-webkit-scrollbar-track { box-shadow: inset 0 0 6px ${accent}; }
+      ::-webkit-scrollbar-thumb { background-color: ${primary}; outline: 1px solid ${secondary}; }
+    `)
+  }
+  
+    //get Style element
+  function getStyleEl () {
+    return document.querySelector(".eda-chrome-extension-styles")
+  }
+};
 
 
   /*-   -   -   -   -   -*/
   /*        Themes       */
   /*-   -   -   -   -   -*/
-
- // Dark Theme 
-function changeDark() {
-  let primary = `#555`;
-  let secondary = `#111`;
-  let tertiary = `#ddd`;
-  let accent = `#fff`;
-  let textPrimary = `#ccc`;
-  let textSecondary = `#ccc`;
-  const styleElement = document.createElement("style");
-  styleElement.setAttribute("id", "eda-chrome-extension-styles");
-  styleElement.innerHTML = `
-  body, main {
-    background: ${secondary} !important;
-  }
-
-  .navBarDefault, aside, .previousBtn, .nextBtn {
-    background: ${primary} !important
-  }
-
-  p , li, ul, a, h1, h2, h3, h4, h5 {
-    color: ${textSecondary} !important;
-  }
-
-  table, thead, tr, td, th {
-    background: ${secondary};
-    color: ${textPrimary};
-  }
-
-  ::-webkit-scrollbar {
-    width: 1em;
-    background: ${tertiary};
-  }
-   
-  ::-webkit-scrollbar-track {
-    // box-shadow: inset 0 0 6px ${accent};
-  }
-   
-  ::-webkit-scrollbar-thumb {
-    background-color: ${primary};
-    outline: 1px solid ${secondary};
-  }
-
-  `;
-  document.getElementsByTagName("head")[0].appendChild(styleElement);
+ 
+const darkTheme = {
+  primary : `#555`,
+  secondary : `#111`,
+  tertiary : `#ddd`,
+  accent : `#fff`,
+  textPrimary : `#ccc`,
+  textSecondary : `#ccc`,
 }
 
-// Light / default theme
-function changeLight() {
-  document.getElementById("eda-chrome-extension-styles").remove();
+const midnightTheme = {
+  primary : `#555`,
+  secondary : `#111`,
+  tertiary : `#ddd`,
+  accent : `#fff`,
+  textPrimary : `#ccc`,
+  textSecondary : `#ccc`,
 }
 
-// Selected Colour Theme
-function changeSelectedColor(hValue) {
-  //define colours
-  let primary = `hsl(${hValue}deg, 100%, 10%)`;
-  let secondary = `hsl(${hValue}deg, 20%, 10%)`;
-  let tertiary = `hsl(${hValue}deg, 5%, 50%)`;
-  let accent = `hsl(${hValue}deg, 80%, 50%)`;
-  let textPrimary = `hsl(${hValue}deg, 0%, 100%)`;
-  let textSecondary = `hsl(${hValue}deg, 0%, 80%)`;
-
-  if (!document.getElementById("eda-chrome-extension-styles")) {
-    const styleElement = document.createElement("style");
-    styleElement.setAttribute("id", "eda-chrome-extension-styles");
-    document.getElementsByTagName("head")[0].appendChild(styleElement);
-  }
-
-  document.getElementById("eda-chrome-extension-styles").innerHTML = `
-  body, main {
-    background: ${secondary} !important;
-  }
-
-  .navBarDefault, aside {
-    background: ${primary} !important;
-    background-color: ${primary} !important;
-  }
-
-  p , li, ul, a, h1, h2, h3, h4, h5 {
-    color: ${textSecondary} !important;
-  }
-
-  table, thead, tr, td, th {
-    background: ${secondary};
-    color: ${textPrimary};
-  }
-
-  ::-webkit-scrollbar {
-    width: 1em;
-    background: ${tertiary};
-  }
-   
-  ::-webkit-scrollbar-track {
-    // box-shadow: inset 0 0 6px ${accent};
-  }
-   
-  ::-webkit-scrollbar-thumb {
-    background-color: ${primary};
-    outline: 1px solid ${secondary};
-  }
-  `;
+function colorThemeTemplate (hValue) {
+  // create colours based on hValue from slider
+  let theme = {
+    primary : `hsl(${hValue}deg, 100%, 10%)`,
+    secondary : `hsl(${hValue}deg, 20%, 10%)`,
+    tertiary : `hsl(${hValue}deg, 5%, 50%)`,
+    accent : `hsl(${hValue}deg, 80%, 50%)`,
+    textPrimary : `hsl(${hValue}deg, 0%, 100%)`,
+    textSecondary : `hsl(${hValue}deg, 0%, 80%)`,
+  };
+  // use default css template to create themeCSS
+  return createCSS(theme)
 }
-
-  /*-   -   -   -   -   -   -   - */
-  /*     Helper/Util Functions    */
-  /*-   -   -   -   -   -   -   - */
-
-
